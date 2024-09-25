59708e99b1146803bc69bf5c0ffc47c9c88dbab0
Humna Imran <168326829+hamnasz@users.noreply.github.com>
2024-09-25 14:45:04 +0500

    Created my first program in DSA Lab

---
+++ Lab/Lab1/ProgramNo1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string grocery[5] = {"bread", "jam", "milk", "butter", "rice"};
+    for (int i = 0; i < 5; i++)
+    {
+        cout << " " << grocery[i];
+    }
+
+}
